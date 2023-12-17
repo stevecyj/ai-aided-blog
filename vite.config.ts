58aaa4dd2f57d8504732034590aabfbb51d9bfa0
stevecyj <ycsteven.tsao@gmail.com>
2023-12-17 23:08:37 +0800

    feat: update TypeScript configuration and add new files and imports
    - Add a new file `auto-imports.d.ts`
    - Add a new file `components.d.ts`
    - Modify `src/main.ts`: remove imports for `element-plus` and its CSS
    - Modify `src/views/member/MemberIndex.vue`: add code for rendering buttons
    - Modify `tsconfig.json`: add type declaration for `element-plus/global`
    - Modify `vite.config.ts`: add plugins for `unplugin-auto-import` and `unplugin-vue-components`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig, normalizePath } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import path from "path";
 import viteEslint from "vite-plugin-eslint";
 import viteStylelint from "vite-plugin-stylelint";
@@ -14,6 +17,12 @@ export default defineConfig({
     viteEslint(),
     viteStylelint({
       exclude: ["node_modules/**"]
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
     })
   ],
   css: {

f1172679e62b7259c886cfb44bc2bdd1b1eed1b8
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-19 11:29:24 +0800

    feat: add error handling and comment API functions
    - Add error handling to the `createBlog` function
    - Add error handling to the `editBlog` function
    - Add error handling to the `deleteBlog` function
    - Add error handling to the `getBlogDetails` function
    - Add error handling to the `likeArticle` function
    - Add error handling to the `unlikeArticle` function
    - Add error handling to the `favoriteArticle` function
    - Add error handling to the `unfavoriteArticle` function
    - Add a new file `commentApi.ts` to handle comment-related API functions
    - Add the `commentArticle` function to handle commenting on an article
    - Add the `deleteComment` function to handle deleting a comment
    - Add the `getCommentsForArticle` function to retrieve comments for an article
    - Add the `likeComment` function to handle liking a comment
    - Add the `unlikeComment` function to handle unliking a comment
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/api/blog/blogApi.ts
+++ src/api/blog/blogApi.ts
@@ -7,7 +7,12 @@ import { type AxiosResponse } from "axios";
 export const createBlog = async (
   blogData: Record<string, unknown>
 ): Promise<AxiosResponse> => {
-  return await axios.post("/api/create-blog", blogData);
+  try {
+    return await axios.post("/api/create-blog", blogData);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 // 編輯部落格
@@ -15,47 +20,82 @@ export const editBlog = async (
   blogId: number,
   blogData: Record<string, unknown>
 ): Promise<AxiosResponse> => {
-  return await axios.put(`/api/edit-blog/${blogId}`, blogData);
+  try {
+    return await axios.put(`/api/edit-blog/${blogId}`, blogData);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 // 刪除部落格
 export const deleteBlog = async (blogId: number): Promise<AxiosResponse> => {
-  return await axios.delete(`/api/delete-blog/${blogId}`);
+  try {
+    return await axios.delete(`/api/delete-blog/${blogId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 // 獲取部落格詳細信息
 export const getBlogDetails = async (
   blogId: number
 ): Promise<AxiosResponse> => {
-  return await axios.get(`/api/blog-details/${blogId}`);
+  try {
+    return await axios.get(`/api/blog-details/${blogId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 // 文章點讚
 export const likeArticle = async (
   articleId: number
 ): Promise<AxiosResponse> => {
-  return await axios.post(`/api/like-article/${articleId}`);
+  try {
+    return await axios.post(`/api/like-article/${articleId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 // 文章取消點讚
 export const unlikeArticle = async (
   articleId: number
 ): Promise<AxiosResponse> => {
-  return await axios.delete(`/api/unlike-article/${articleId}`);
+  try {
+    return await axios.delete(`/api/unlike-article/${articleId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 // 文章收藏
 export const favoriteArticle = async (
   articleId: number
 ): Promise<AxiosResponse> => {
-  return await axios.post(`/api/favorite-article/${articleId}`);
+  try {
+    return await axios.post(`/api/favorite-article/${articleId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 // 文章取消收藏
 export const unfavoriteArticle = async (
   articleId: number
 ): Promise<AxiosResponse> => {
-  return await axios.delete(`/api/unfavorite-article/${articleId}`);
+  try {
+    return await axios.delete(`/api/unfavorite-article/${articleId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 // 其他部落格相關的 API 函數，根據需求添加
---
+++ src/api/comment/commentApi.ts
@@ -0,0 +1,75 @@
+import axios from "@/api/axios-instance"; // 導入之前封裝的 Axios 實例
+import { type AxiosResponse } from "axios";
+
+// 定義評論相關的 API 接口
+
+// 評論文章
+export const commentArticle = async (
+  articleId: number,
+  commentData: Record<string, unknown>
+): Promise<AxiosResponse> => {
+  try {
+    return await axios.post(`/api/comment-article/${articleId}`, commentData);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// 刪除評論
+export const deleteComment = async (
+  commentId: number
+): Promise<AxiosResponse> => {
+  try {
+    return await axios.delete(`/api/delete-comment/${commentId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// 獲取文章評論列表
+export const getCommentsForArticle = async (
+  articleId: number
+): Promise<AxiosResponse> => {
+  try {
+    return await axios.get(`/api/comments-for-article/${articleId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// 評論點讚
+export const likeComment = async (
+  commentId: number
+): Promise<AxiosResponse> => {
+  try {
+    return await axios.post(`/api/like-comment/${commentId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// 評論取消點讚
+export const unlikeComment = async (
+  commentId: number
+): Promise<AxiosResponse> => {
+  try {
+    return await axios.delete(`/api/unlike-comment/${commentId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// 其他評論相關的 API 函數，根據需求添加
+
+export default {
+  commentArticle,
+  deleteComment,
+  getCommentsForArticle,
+  likeComment,
+  unlikeComment
+};
